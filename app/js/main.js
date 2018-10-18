aa0be4f5f13aea88be57b77067795db71c96d21f
MarMilli <marinemillionaire@gmail.com>
2018-10-18 17:30:22 +0700

    добавила добавление в корзину и замену текста

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
 
 
+    //функция добавления товара в корзину
+    function addCart(add){
+        var block = add.parents(".block"); //ищем родителя блока
+        if ((block).hasClass("notActive") ) //если блок неактивен
+            return; //завершаем работу
+        block.toggleClass('choose'); //иначе добавляем ему класс
+
+    }
+    //вызов функции добавления товара в корзину при клике на карточку
+    $(function(){
+        $('.blockCart').click(function(){
+            addCart($(this));
+        });
+    });
+    //вызов функции добавления товара при клике на кнопку
+    $(function(){
+        $('.basketAdd__link').click(function(){
+            addCart($(this));
+        });
+    });
+
+
+    $(".block").each(function () {
+
+        if ($(this).hasClass("notActive")) {  //если блок неактивен
+            var filling = $(this).find('.subsidiary').text();
+            $(this).find('.disabled').html("Печалька, " + filling + " закончился.");
+        }
+    });
 });
 
