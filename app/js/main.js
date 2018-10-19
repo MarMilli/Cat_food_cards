59462ca48a04c1499d74b63c19f3e4cc7fb3d1a3
MarMilli <marinemillionaire@gmail.com>
2018-10-19 17:04:14 +0700

    доделала состояние наведения я к выбранной упаковке

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,50 @@
 $(document).ready(function () {
 
-
     //функция добавления товара в корзину
-    function addCart(add){
+    function addCart(add) {
         var block = add.parents(".block"); //ищем родителя блока
-        if ((block).hasClass("notActive") ) //если блок неактивен
+        var searchBlockText = block.find('.dataText');//
+        if ((block).hasClass("notActive")) //если блок неактивен
             return; //завершаем работу
         block.toggleClass('choose'); //иначе добавляем ему класс
+        var dataText = block.find('[data-text]').attr('data-text');
+        var existingText = block.find('.dataText').text();
 
+        //замена текста в шапке карточки
+        function change() {
+            searchBlockText
+                .toggleClass("question")
+                .text(dataText)
+                .attr('data-text', existingText);
+        }
+
+        //поменять текст если пользователь убрал курсор
+        block.on("mouseleave", function () {
+            if (((block).hasClass("choose")) && (!(searchBlockText.hasClass("question")))) {
+                change();
+            }
+        });
+
+        //обратно поменять текст при повторном клике
+        if ((!((block).hasClass("choose"))) && (searchBlockText.hasClass("question"))) {
+            change();
+        }
     }
+
     //вызов функции добавления товара в корзину при клике на карточку
-    $(function(){
-        $('.blockCart').click(function(){
+    $(function () {
+        $('.blockCart').click(function () {
             addCart($(this));
         });
     });
     //вызов функции добавления товара при клике на кнопку
-    $(function(){
-        $('.basketAdd__link').click(function(){
+    $(function () {
+        $('.basketAdd__link').click(function () {
             addCart($(this));
         });
     });
-
-
+    //функция для неактивного блока
     $(".block").each(function () {
-
         if ($(this).hasClass("notActive")) {  //если блок неактивен
             var filling = $(this).find('.subsidiary').text();
             $(this).find('.disabled').html("Печалька, " + filling + " закончился.");
